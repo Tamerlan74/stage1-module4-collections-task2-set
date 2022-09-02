adc9fc6df551ea994bff18a9c8c5e1fc6b636435
Tamerlan Zhabassov <zhbssv@mail.ru>
2022-09-02 19:24:19 +0600

    do

--- src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
+++ src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
@@ -1,9 +1,37 @@
 package com.epam.mjc.collections.set;
 
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 
 public class HashSetCreator {
     public HashSet<Integer> createHashSet(List<Integer> sourceList) {
+        HashSet<Integer> result = new HashSet<>();
+        for (int i = 0; i < sourceList.size(); i++) {
+            result.addAll(addNumber(sourceList.get(i)));
+        }
+        return result;
+    }
+
+    private HashSet<Integer> addOddNumber(int n) {
+        Collection<Integer> numbers = List.of(n, n * 2);
+        return new HashSet<>(numbers);
+    }
+
+    private HashSet<Integer> addEvenNumber(int n) {
+        HashSet<Integer> result = new HashSet<>();
+        result.add(n);
+        if (n % 2 == 0) {
+            result.addAll(addEvenNumber(n / 2));
+        }
+        return result;
+    }
+
+    private HashSet<Integer> addNumber(int n) {
+        if (n % 2 == 0) {
+            return addEvenNumber(n);
+        } else {
+            return addOddNumber(n);
+        }
     }
 }
--- src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
@@ -5,5 +5,14 @@ import java.util.Set;
 
 public class SetCombinationCreator {
     public Set<String> createSetCombination(Set<String> firstSet, Set<String> secondSet, Set<String> thirdSet) {
+        HashSet<String> result = new HashSet<>();
+        result.addAll(firstSet);
+        result.addAll(secondSet);
+        result.addAll(thirdSet);
+        result.removeAll(firstSet);
+        result.removeAll(secondSet);
+        firstSet.retainAll(secondSet);
+        result.addAll(firstSet);
+        return result;
     }
 }
